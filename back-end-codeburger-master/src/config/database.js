a777b2b8f450eafc6fdb16df5b33038be829c1e8
Joao-Iray <joao.iray1@outlook.com>
2025-01-21 22:11:45 -0300

    devburguer-back

---
+++ back-end-codeburger-master/src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'admin',
+  database: 'codeburger',
+  define: {
+    timespamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+}
